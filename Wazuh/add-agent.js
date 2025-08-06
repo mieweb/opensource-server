fa12eda35083c2e36e53c812dd9b66c79420ed39
maxklema <maxklema@gmail.com>
2025-08-06 18:59:50 -0400

    wazuh agent creation script

---
+++ Wazuh/add-agent.js
@@ -0,0 +1,47 @@
+// Script to add a Wazuh Agent to the Wazuh Manager
+// Last Modified on August 6th, 2025 by Maxwell Klema
+// -------------------------------------------------
+
+const axios = require('axios');
+const env = require('dotenv').config();
+
+let config = {
+    method: 'post',
+    url: 'https://wazuh-server.opensource.mieweb.org/security/user/authenticate',
+    maxBodyLength: Infinity,
+    headers: {
+        'Authorization': `Basic ${Buffer.from(`${process.env.API_USERNAME}:${process.env.API_PASSWORD}`).toString('base64')}`,
+    }
+}
+
+axios.request(config).then((response) => {
+    if (response.status != 200) {
+        console.error('Failed to authenticate with Wazuh API');
+        return;
+    }
+
+    const JWT = response.data.data.token;
+
+    // Add the Agent to the Manager
+    let agentConfig = {
+        method: 'post',
+        url: 'https://wazuh-server.opensource.mieweb.org/agents?pretty=true',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${JWT}`
+        },
+        data: {
+            'name': 'test-container-max-2',
+            'ip': '10.15.190.52'
+        }
+    }
+
+    axios.request(agentConfig).then((response) => {
+        if (response.status !== 200) {
+            console.error('Failed to add agent:', response.data);
+        }
+
+        agentKey = response.data.data.key
+        console.log('Agent added successfully:', agentKey);
+    })
+});
