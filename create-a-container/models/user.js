eae4f5eacd82ac40f6f651d45cd3249592590302
Robert Gingras <rgingras@mieweb.com>
2025-11-21 16:58:54 -0500

    update user model with password hashing logic

--- create-a-container/models/user.js
+++ create-a-container/models/user.js
@@ -2,6 +2,8 @@
 const {
   Model
 } = require('sequelize');
+const argon2 = require('argon2');
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -12,12 +14,31 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // Define associations here if needed in the future
     }
+
+    /**
+     * Gets the next available UID number by finding the max and adding 1
+     * @returns {Promise<number>} - The next available UID number
+     */
+    static async nextUidNumber() {
+      const maxUidNumber = await User.max('uidNumber');
+      // If no users exist, start at 1001 (common starting point for user UIDs)
+      return maxUidNumber ? maxUidNumber + 1 : 2000;
+    }
+
+    /**
+     * Validates a plaintext password against the stored encrypted password
+     * @param {string} plainPassword - The plaintext password to validate
+     * @returns {boolean} - True if the password matches, false otherwise
+     */
+    async validatePassword(plainPassword) {
+      return await argon2.verify(plainPassword, this.userPassword);
+    }
   }
   User.init({
     uidNumber: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      primaryKey: true
+      primaryKey: true,
     },
     uid: {
       type: DataTypes.STRING(255),
@@ -26,19 +47,21 @@ module.exports = (sequelize, DataTypes) => {
     },
     gidNumber: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      defaultValue: 2001
     },
     homeDirectory: {
       type: DataTypes.STRING(255),
-      allowNull: false
+      allowNull: false,
     },
     loginShell: {
       type: DataTypes.STRING(255),
-      allowNull: false
+      allowNull: false,
+      defaultValue: '/bin/bash'
     },
     cn: {
       type: DataTypes.STRING(255),
-      allowNull: false
+      allowNull: false,
     },
     sn: {
       type: DataTypes.STRING(255),
@@ -59,7 +82,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     userPassword: {
       type: DataTypes.STRING(255),
-      allowNull: false
+      allowNull: false,
     },
     status: {
       type: DataTypes.STRING(50),
@@ -70,7 +93,17 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'User',
     tableName: 'Users',
-    timestamps: true
+    timestamps: true,
+    hooks: {
+      beforeCreate: async (user, options) => {
+        user.userPassword = await argon2.hash(user.userPassword);
+      },
+      beforeUpdate: async (user, options) => {
+        if (user.changed('userPassword')) {
+          user.userPassword = await argon2.hash(user.userPassword);
+        }
+      }
+    }
   });
   return User;
 };
