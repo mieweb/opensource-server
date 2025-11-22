48e73cda97d9c0191fce94b21dbc40815b9bb1e2
Robert Gingras <rgingras@mieweb.com>
2025-11-21 19:05:37 -0500

    refactor: simplify req handling in templates

--- create-a-container/server.js
+++ create-a-container/server.js
@@ -113,7 +113,7 @@ app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`)
 
 // Serves the main container creation form
 app.get('/containers/new', requireAuth, (req, res) => {
-  res.render('containers/form');
+  res.render('containers/form', { req });
 });
 
 // Fetch user's containers
@@ -150,9 +150,7 @@ app.get('/containers', requireAuth, async (req, res) => {
 
   return res.render('containers/index', { 
     rows,
-    isAdmin: req.session.isAdmin || false,
-    successMessages: req.flash('success'),
-    errorMessages: req.flash('error')
+    req
   });
 });
 
