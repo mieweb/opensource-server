e65f46718edf6ee465bbd96472b8ef351a3c5daf
Arsh Sandhu <sanda09@pfw.edu>
2026-07-17 10:59:38 -0400

    feat(containers): store provider container ids as strings

---
+++ create-a-container/migrations/20260717130000-change-container-id-to-string.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.changeColumn('Containers', 'containerId', {
+      type: Sequelize.STRING(255),
+      allowNull: true,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.changeColumn('Containers', 'containerId', {
+      type: Sequelize.INTEGER.UNSIGNED,
+      allowNull: true,
+    });
+  },
+};
